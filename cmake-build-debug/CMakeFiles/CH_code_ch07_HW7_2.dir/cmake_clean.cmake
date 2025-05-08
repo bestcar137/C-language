2d9a8c122aa9a19d7c04c3ade8075d36f96ce32a
bestcar137 <bestcar137@gmail.com>
2025-05-08 16:30:46 +0900

    20250508 ch07 HW

---
+++ cmake-build-debug/CMakeFiles/CH_code_ch07_HW7_2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CH_code_ch07_HW7_2.exe"
+  "CH_code_ch07_HW7_2.exe.manifest"
+  "CH_code_ch07_HW7_2.pdb"
+  "CMakeFiles/CH_code_ch07_HW7_2.dir/CH_code/ch07/HW7_2.c.obj"
+  "CMakeFiles/CH_code_ch07_HW7_2.dir/CH_code/ch07/HW7_2.c.obj.d"
+  "libCH_code_ch07_HW7_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CH_code_ch07_HW7_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CH_code_ch07_HW7_2.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for CH_code_ch07_HW7_2.
+# This may be replaced when dependencies are built.
